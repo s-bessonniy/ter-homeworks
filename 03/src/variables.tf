e577110afac4d103d8a407cbe94fd295571eb2af
s-bessonniy <164614779+s-bessonniy@users.noreply.github.com>
2024-11-10 15:31:31 +0300

    Update variables.tf

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -2,16 +2,22 @@
 variable "token" {
   type        = string
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  #default = ""
+  #sensitive = true
 }
 
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  #default = ""
+  #sensitive = true
 }
 
 variable "folder_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  #default = ""
+  #sensitive = true
 }
 
 variable "default_zone" {
@@ -29,4 +35,55 @@ variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+
+variable "vm_image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "yandex_compute_image_family"
+}
+
+variable "vm_resources" {
+  type = map
+  default = {
+  cores = "2" 
+  memory = "1"
+  core_fraction = "20"
+  }
+}
+
+variable "vms_ssh_root_key" {
+  description = "metadata for all vms"
+  type        = map(string)
+  default     = {
+    serial-port-enable = "1"
+    ssh-keys           = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIgCTAknACY9siTMrK+ozJsJoFis+9ePIUyAC8YYd/K s_yaremko@Ubuntu-50Gb"
+  }
+}
+
+variable "vm_datebase" {
+  type = list(object({
+    vm_name       = string
+    cpu           = number
+    ram           = number
+    core_fraction = number
+    disk_size     = number
+  }))
+  default = [
+    {
+      vm_name       = "main"
+      cpu           = 2
+      ram           = 1
+      core_fraction = 20
+      disk_size     = 10  
+    },
+    { vm_name       = "replica"
+      cpu           = 2
+      ram           = 1
+      core_fraction = 20
+      disk_size     = 20
+    }
+
+  ]
+
 }
