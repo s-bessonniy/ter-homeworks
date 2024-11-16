380143c411856ce5ca7742988db70908598d1833
s-bessonniy <164614779+s-bessonniy@users.noreply.github.com>
2024-11-16 13:55:34 +0300

    Update disk_vm.tf

--- 03/src/disk_vm.tf
+++ 03/src/disk_vm.tf
@@ -1,18 +1,18 @@
 resource "yandex_compute_disk" "default_disk" {
-  count      = 3
-  name       = "default-disk-${count.index + 1}"
-  type       = "network-hdd"
+  count      = var.disk_resources.count
+  name       = "${var.disk_names}-${count.index + 1}"
+  type       = var.disk_resources.type
   zone       = var.default_zone
-  size       = 1
-  block_size = 4096
+  size       = var.disk_resources.size
+  block_size = var.disk_resources.block_size
 }
 
 resource "yandex_compute_instance" "storage_server" {
 
     depends_on = [yandex_compute_disk.default_disk]
 
-  name        = "storage"
-  platform_id = "standard-v3"
+  name        = var.server_disk_names
+  platform_id = var.vm_resources.platform_id
 
   resources {
     cores         = var.vm_resources.cores
@@ -30,21 +30,21 @@ boot_disk {
     for_each = toset(yandex_compute_disk.default_disk[*].id)
     content {
       disk_id     = secondary_disk.key
-      auto_delete = true
+      auto_delete = var.vm_def
     }
   }
 
 metadata = var.vms_ssh_root_key
 
 scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_def
   }
 
    network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_def
   }
 
- allow_stopping_for_update = true
+ allow_stopping_for_update = var.vm_def
 
 }
