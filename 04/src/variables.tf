49b79a4e4f4a5079369170fdede76d0457321bec
s-bessonniy <164614779+s-bessonniy@users.noreply.github.com>
2024-11-18 08:41:48 +0300

    Update variables.tf

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -14,14 +14,25 @@ variable "folder_id" {
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
-variable "default_zone" {
+variable "default_zone_a" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+variable "default_zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr_zone_a" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "default_cidr_zone_b" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
@@ -33,25 +44,58 @@ variable "vpc_name" {
 
 ###common vars
 
-variable "vms_ssh_root_key" {
+variable "public_key" {
   type        = string
-  default     = "your_ssh_ed25519_key"
-  description = "ssh-keygen -t ed25519"
+  default     = "~/.ssh/id_ed25519.pub"
+
 }
 
-###example vm_web var
-variable "vm_web_name" {
+variable "username_init" {
   type        = string
-  default     = "netology-develop-platform-web"
-  description = "example vm_web_ prefix"
+  default     = "ubuntu"
+
 }
 
-###example vm_db var
-variable "vm_db_name" {
-  type        = string
-  default     = "netology-develop-platform-db"
-  description = "example vm_db_ prefix"
+variable "vm_marketing" {
+  type = object({
+    env_name       = string
+    instance_name  = string
+    instance_count = number
+    image_family   = string
+    public_ip      = bool
+    owner          = string
+    project        = string
+  })
+  default = {
+    env_name       = "develop-marketing"
+    instance_name  = "web-marketing"
+    instance_count = 1
+    image_family   = "ubuntu-2004-lts"
+    public_ip      = true
+    owner          = "s_yaremko"
+    project        = "marketing"
+  }
 }
 
 
+variable "vm_analytics" {
+  type = object({
+    env_name       = string
+    instance_name  = string
+    instance_count = number
+    image_family   = string
+    public_ip      = bool
+    owner          = string
+    project        = string
+  })
+  default = {
+    env_name       = "develop-analytics"
+    instance_name  = "web-analytics"
+    instance_count = 1
+    image_family   = "ubuntu-2004-lts"
+    public_ip      = true
+    owner          = "s_yaremko"
+    project        = "analytics"
+  }
+}
 
